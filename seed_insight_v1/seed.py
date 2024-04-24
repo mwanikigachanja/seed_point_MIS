d9bd9fd55946aa3838e52c5384637cef8d64c24c
Mwaniki Charles <mwanikigachanja@gmail.com>
2024-04-23 23:30:51 -0700

    Seed module update

--- seed_insight_v1/seed.py
+++ seed_insight_v1/seed.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/seedinsight'
@@ -14,12 +15,18 @@ class Seed(db.Model):
 @app.route('/seeds', methods=['POST'])
 def add_seed():
     data = request.json
-    name = data['name']
-    variety = data['variety']
-    # Add more fields as needed
+    name = data.get('name')
+    variety = data.get('variety')
+    # Input validation
+    if not name or not variety:
+        return jsonify({'message': 'Name and variety are required'}), 400
     new_seed = Seed(name=name, variety=variety)
-    db.session.add(new_seed)
-    db.session.commit()
+    try:
+        db.session.add(new_seed)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'message': 'Seed name must be unique'}), 400
     return jsonify({'message': 'Seed added successfully'}), 201
 
 @app.route('/seeds', methods=['GET'])
